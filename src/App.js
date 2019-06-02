33d7c55c29b1ec4e15e7c5f42740295847ec286e
John Ray <john@johnraymon.com>
2019-06-02 05:42:41 -0400

    implement App container, create initial state, make initial call when component mounts to update initial state, implement UI to render Cart component, and Products component based on state. implement methods to give behavior to Cart and Products components

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+
+// components
+import Product from './Product'
+import Cart from './Cart'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.addProductToCart = this.addProductToCart.bind(this)
+    this.removeProductFromCart = this.removeProductFromCart.bind(this)
+    this.state = {
+      products: [],
+      cart: null,
+      productsError: null
+    }
+  }
+  componentDidMount() {
+    const {
+      commerce
+    } = this.props
+    if (commerce !== undefined && typeof commerce !== 'undefined') {
+      // invoke commerce products method to get all products
+      commerce.Products.list(
+        (resp) => {
+          //Success
+          this.setState({
+            productsError: null,
+            products: resp.data
+          })
+        },
+        (error) => {
+          //Error handler
+          this.setState({
+            productsError: error.message || `We apologize, we're experiencing technical difficulties`
+          })
+        }
+      );
+      window.addEventListener("Commercejs.Cart.Ready", function (e) {
+        // invoke commerce cart method to retrieve cart in session
+        // debugger;
+        commerce.Cart.retrieve((cart) => {
+            return this.setState({
+              cart: cart
+            })
+          });
+      }.bind(this))
+
+    }
+  }
+  // product methods
+  addProductToCart(productId) {
+    this.props.commerce.Cart.add({
+      id: productId,
+    }, (resp) => {
+      // if successful update Cart
+      if (!resp.error) {
+        this.setState({
+          cart: resp.cart
+        })
+      }
+    });
+  }
+  // cart methods
+  removeProductFromCart(lineItemId) {
+    this.props.commerce.Cart.remove(lineItemId, (resp) => {
+      // if successful update Cart
+      if (!resp.error) {
+        this.setState({
+          cart: resp.cart
+        })
+      }
+    });
+  }
+  render() {
+    const {
+      products,
+      cart
+    } = this.state;
+    const allProducts = products.map((product, key) => {
+      if (product.active) {  // the item is active and isn't sold out so display it
+        return (
+          <Product {...product} addProductToCart={this.addProductToCart} key={key} />
+        )
+      }
+    })
+    return (
+      <div className="App ph2 pv4">
+        <Cart
+          cart={cart}
+          removeProductFromCart={this.removeProductFromCart}
+        />
+        <div className="products-container mw7 center cf">
+          <h2 className="tracked ttu gray">
+            All Products
+          </h2>
+          { allProducts.length ?
+            allProducts :
+            'There no products available right now'
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
