a587e43767a566cc89fc3417ee6f5c9267e5f2b6
John Ray <john@johnraymon.com>
2019-06-02 05:55:16 -0400

    implement LineItem component to represent line item in cart, with proper details from prop, and behavior

---
+++ src/LineItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function LineItem({ id, name, quantity, removeProductFromCart }) {
+  return (
+    <div className="cart-container__line-item w-100 flex items-center justify-between pb2">
+      <div className="w-auto pr1">
+        <p className="tracked ttc f7">
+          {name} - ({quantity})
+        </p>
+      </div>
+      <button className="b1 b--mid-gray outline-0 pointer pa2" onClick={() => removeProductFromCart(id)}>
+        Remove from Cart
+      </button>
+    </div>
+  )
+}
+
+export default LineItem;
