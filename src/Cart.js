8f7438042715dd403edc93db6418669092b96b3d
John Ray <john@johnraymon.com>
2019-06-02 05:54:45 -0400

    implement Cart container, handle for when cart is still null, meaning it still hasn't loaded completely, so we fall back to loading, map a new array with jsx elements representing the lineitems, using LineItem component

---
+++ src/Cart.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+// components
+import LineItem from './LineItem';
+
+function Cart({ cart, removeProductFromCart }) {
+    if (cart) {
+      const {
+        total_items: totalItems,
+        subtotal,
+        line_items: lineItems
+      } = cart;
+
+      const allLineItems = lineItems.map((item, key) => {
+        return (
+          <li key={key}>
+            <LineItem {...item} removeProductFromCart={removeProductFromCart} />
+          </li>
+        )
+      })
+    return (
+      <div className="cart-container mw7 center">
+        <h2 className="tracked ttu gray flex justify-between">
+          Cart ({totalItems})
+          <span className="flex flex-column f7">
+            <span>
+              subtotal: {subtotal.formatted_with_code}
+            </span>
+          </span>
+        </h2>
+          {(totalItems > 0) ?
+          (
+            <ul className="w-100 list-reset ma0 pv0 ph4">
+              { allLineItems }
+            </ul>
+          ) :
+          (
+            <p className="f7 tracked">Your cart is empty</p>
+          )}
+      </div>
+    )
+  } else {
+    return (
+      <p>Loading Cart ...</p>
+    )
+  }
+}
+
+export default Cart;
