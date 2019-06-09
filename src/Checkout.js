6fb9302375998c588e1872ed2502a170c9ecff9e
John Ray <john@johnraymon.com>
2019-06-09 19:13:16 -0400

    create src/Checkout: implement Checkout component

---
+++ src/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <div className="cart-container mw7 center">
+      <h2 className="tracked ttu gray flex justify-between">
+        Checkout
+      </h2>
+
+    </div>
+  )
+}
+
+
+export default Checkout
