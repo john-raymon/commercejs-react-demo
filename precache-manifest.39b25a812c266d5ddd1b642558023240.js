046884355de47ef38ccc5a6cf0599ced6ac7d3f9
John Ray <john@johnraymon.com>
2019-06-10 08:53:46 -0400

    Updates

---
+++ precache-manifest.39b25a812c266d5ddd1b642558023240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d57d70c6f2febbb3fdec118a0566f0d",
+    "url": "/commercejs-react-demo/index.html"
+  },
+  {
+    "revision": "8f5f945acae21b5463a3",
+    "url": "/commercejs-react-demo/static/css/2.64221491.chunk.css"
+  },
+  {
+    "revision": "6c61c7b815386a59849a",
+    "url": "/commercejs-react-demo/static/css/main.195b6288.chunk.css"
+  },
+  {
+    "revision": "8f5f945acae21b5463a3",
+    "url": "/commercejs-react-demo/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "6c61c7b815386a59849a",
+    "url": "/commercejs-react-demo/static/js/main.2cc95867.chunk.js"
+  },
+  {
+    "revision": "82aceff5aa18803a7912",
+    "url": "/commercejs-react-demo/static/js/runtime~main.147a5891.js"
+  }
+]);
