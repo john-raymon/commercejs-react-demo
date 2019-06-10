638a8e3526f6cdd0367095d1e65d92350ab7caa9
John Ray <john@johnraymon.com>
2019-06-10 15:57:37 -0400

    update src/LineItem: render cart's line item's subtotal

--- src/LineItem.js
+++ src/LineItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function LineItem({ id, name, quantity, removeProductFromCart }) {
+function LineItem({ id, name, quantity, removeProductFromCart, line_total:subtotal}) {
   return (
     <div className="cart-container__line-item w-100 flex items-center justify-between pb2">
       <div className="w-auto pr1">
         <p className="tracked ttc f7">
-          {name} - ({quantity})
+          {name} - ({quantity}) - ${subtotal.formatted_with_code}
         </p>
       </div>
       <button className="dim b1 b--mid-gray outline-0 pointer pa2" onClick={() => removeProductFromCart(id)}>
